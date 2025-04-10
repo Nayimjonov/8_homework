afe0183fcd86095fa78cd298a47068ec0ecf9568
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 14:22:43 +0500

    done

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,1 +1,16 @@
 from rest_framework import serializers
+from .models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name',
+            'description',
+            'parent',
+            'image',
+            'created_at',
+            'updated_at'
+        )
