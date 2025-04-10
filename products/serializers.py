bbaecf460ab1d9fc7b966e9ee273985d570d56e2
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 15:16:18 +0500

    productc serializer yozildi

--- products/serializers.py
+++ products/serializers.py
@@ -1,1 +1,27 @@
 from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_id = serializers.PrimaryKeyRelatedField(source='category')
+    category_name = serializers.StringRelatedField(source='category', read_only=True)
+
+
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'discount_price',
+            'category_id',
+            'category_name',
+            'category_by_name',
+            'images',
+            'stock',
+            'is_active',
+            'created_at',
+            'updated_at',
+        )
