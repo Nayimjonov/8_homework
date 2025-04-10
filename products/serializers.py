c8ad0483df4db305cd1e3f2fc31b5200d4e8a3cb
Samandar <samandarnayimjonov@gmail.com>
2025-04-10 16:01:16 +0500

    don e

--- products/serializers.py
+++ products/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import Product
+from categories.models import Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_id = serializers.PrimaryKeyRelatedField(source='category')
+    category_id = serializers.PrimaryKeyRelatedField(source='category', queryset=Category.objects.all())
     category_name = serializers.StringRelatedField(source='category', read_only=True)
-
-
+    category_by_name = serializers.CharField(source='category.name', read_only=True)
+    images = serializers.JSONField(required=False)
 
     class Meta:
         model = Product
@@ -25,3 +26,11 @@ class ProductSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         )
+
+    def create(self, validated_data):
+        images = validated_data.pop('images', [])
+        product = Product.objects.create(**validated_data)
+        product.images.set(images)
+        return product
+
+
